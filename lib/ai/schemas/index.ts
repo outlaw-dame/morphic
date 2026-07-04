7ed880da8ac54859aa00d86487277429dbe505f6
Damon O <141657791+outlaw-dame@users.noreply.github.com>
2026-07-03 23:37:10 -0400

    feat: export AI shared schema modules

---
+++ lib/ai/schemas/index.ts
@@ -0,0 +1,35 @@
+export {
+  EvidenceRoleSchema,
+  ModelCapabilitySchema,
+  ModelRoleSchema,
+  ResearchModeSchema,
+  RiskLevelSchema,
+  SourceClassSchema,
+  type EvidenceRole,
+  type ModelCapability,
+  type ModelRole,
+  type ResearchMode,
+  type RiskLevel,
+  type SourceClass
+} from './core'
+export {
+  CoordinatorDecisionSchema,
+  EvidenceItemSchema,
+  RoutePlanSchema,
+  parseCoordinatorDecision,
+  parseEvidenceItem,
+  parseRoutePlan,
+  type CoordinatorDecision,
+  type EvidenceItem,
+  type RoutePlan
+} from './route'
+export {
+  AdvisorFindingSchema,
+  AdvisorSeveritySchema,
+  SourceQualityAssessmentSchema,
+  parseAdvisorFinding,
+  parseSourceQualityAssessment,
+  type AdvisorFinding,
+  type AdvisorSeverity,
+  type SourceQualityAssessment
+} from './review'
