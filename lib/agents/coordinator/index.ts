127ad41735fcc75eb66ecdcb1ac95cd8433a9202
Damon O <141657791+outlaw-dame@users.noreply.github.com>
2026-07-06 08:41:00 -0400

    feat: export coordinator modules

---
+++ lib/agents/coordinator/index.ts
@@ -0,0 +1,5 @@
+export { coordinateExecution } from './coordinator'
+export { createCoordinatorExecutionState } from './execution-state'
+
+export type { CoordinatorEvaluation } from './coordinator'
+export type { CoordinatorExecutionState } from './execution-state'
